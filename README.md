7930e26c5c16e0508ed709e70829484023c7596b
ale-augustin <aleaugus@gmail.com>
2023-10-13 04:08:39 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# PayloadPrettify
+
+`PayloadPrettify`, part of the `payloadTools` package, streamlines the process for developers and analysts working with payload formats like .dat, .cxml, and .idoc. With just a few actions, users can transform these payloads into standardized, readable XML formats directly within Visual Studio Code.
+
+## Features
+
+- **Payload to Prettified XML**: 
+  - Easily convert varying payload formats into a standardized and reader-friendly XML.
+  - The feature first linearizes the XML for improved manageability and then applies thorough XML formatting.
+
+  ![Demonstration](https://imgur.com/a/sgxT95k)
+
+## Requirements
+
+- **XML Tools Dependency**: 
+  - For the optimal functionality of this extension, the installation of [XML Tools](https://marketplace.visualstudio.com/items?itemName=DotJoshJohnson.xml) is mandatory. 
+  - Should you not have it installed, upon installing `PayloadPrettify`, VS Code will prompt you for its installation.
+
+## Extension Settings
+
+As of now, there aren't any user-specific settings required for this extension. All its features are designed to work straight out of the box.
+
+## Known Issues
+
+- **XML Tools Dependency**: 
+  - The proper functioning of `PayloadPrettify` hinges on the continued installation of XML Tools. 
+  - For any issues arising related to XML formatting, ensure XML Tools remains active and properly functioning.
+
+## Release Notes
+
+### Version 1.0.0
+
+- Initial release of `PayloadPrettify`.
+- Introduced features:
+  - Transformation of payload files into standardized XML formats.
+  - Established dependency on XML Tools for enhanced XML formatting.
---
+++ extension.js
@@ -0,0 +1,58 @@
+const vscode = require('vscode');  // Import the required Visual Studio Code module
+const fs = require('fs');          // Import the file system module
+const path = require('path');      // Import the path module
+
+function activate(context) {
+    // Register a new command with the command ID 'payloadtools.payloadprettify.changeToXML'
+    let disposable = vscode.commands.registerCommand('aleaugus.payloadprettify.changeToXML', function () {
+        // Get the active text editor
+        let editor = vscode.window.activeTextEditor;
+
+        // If no document is open, show an error message and return
+        if (!editor) {
+            vscode.window.showErrorMessage('No document open!');
+            return;
+        }
+
+        // Get the current file path, directory, and file name without extension
+        let currentFilePath = editor.document.fileName;
+        let fileDir = path.dirname(currentFilePath);
+        let fileNameWithoutExtension = path.basename(currentFilePath, path.extname(currentFilePath));
+
+        // Construct the new file path with the .xml extension
+        let newFilePath = path.join(fileDir, fileNameWithoutExtension + '.xml');
+
+        // Check if a file with the new name already exists
+        if (fs.existsSync(newFilePath)) {
+            vscode.window.showErrorMessage('A file with the same name and .xml extension already exists.');
+            return;
+        }
+
+        // Rename the current file to the new name with the .xml extension
+        fs.renameSync(currentFilePath, newFilePath);
+
+        // Open the renamed document
+        vscode.workspace.openTextDocument(newFilePath).then((document) => {
+            vscode.window.showTextDocument(document).then((editor) => {
+                // Linearize the content by removing consecutive whitespaces
+                let content = document.getText();
+                let linearizedContent = content.replace(/\s+/g, ' ').trim();
+
+                // Replace the entire content of the document with the linearized content
+                editor.edit(editBuilder => {
+                    let docRange = new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end);
+                    editBuilder.replace(docRange, linearizedContent);
+                }).then(() => {
+                    // After linearizing, format the content using the XML Tools extension
+                    vscode.commands.executeCommand('xmlTools.formatAsXml');
+                });
+            });
+        });
+    });
+
+    // Add the command to the extension's context so it gets disposed of correctly
+    context.subscriptions.push(disposable);
+}
+
+// Export the activate function so it can be used by VS Code
+exports.activate = activate;
